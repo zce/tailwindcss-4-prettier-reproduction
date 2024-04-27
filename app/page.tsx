c3d1bd984ac4fc89ac4257e1371d7e8859155bb4
zce <w@zce.me>
2024-04-27 11:21:31 +0800

    feat: prettier tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="border-subtle border text-3xl font-bold underline ring">Hello world!</h1>
+  return <h1 className="border border-subtle text-3xl font-bold underline ring">Hello world!</h1>
 }
